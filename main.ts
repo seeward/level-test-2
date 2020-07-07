cc5f2b598a6f612865844650f62d3d09b1b4363d
Christian McCabe <seeward@gmail.com>
2020-07-07 19:27:33 +0200

    added

--- main.ts
+++ main.ts
@@ -20,7 +20,8 @@ namespace myTiles {
 `
 }
 function makeDemon () {
-    mySprite = sprites.create(img`
+    if (bossMade == 0) {
+        mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -46,10 +47,10 @@ function makeDemon () {
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
-    mySprite.follow(jesus.sprite, 20)
-    animation.runImageAnimation(
-    mySprite,
-    [img`
+        mySprite.follow(jesus.sprite, 20)
+        animation.runImageAnimation(
+        mySprite,
+        [img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -150,10 +151,11 @@ function makeDemon () {
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 `],
-    500,
-    true
-    )
-    tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles10)
+        500,
+        true
+        )
+        tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles10)
+    }
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.WalkLeft)
@@ -168,16 +170,166 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
     firing = 0
 })
+function makeBossDemon () {
+    if (bossMade == 0) {
+        bossDemon = sprites.create(img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`, SpriteKind.Enemy)
+        animation.runImageAnimation(
+        bossDemon,
+        [img`
+. . . c c c c c c . . . . . . . 
+. . c 6 7 7 7 7 6 c . . . . . . 
+. c 7 7 7 7 7 7 7 7 c . . . . . 
+c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+. f 7 7 7 7 6 c 7 7 6 f . . . . 
+. . f c c c c 7 7 6 f c c c . . 
+. . c 6 2 7 7 7 f c c 7 7 7 c . 
+. c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+. c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+. c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+. . c 6 1 1 1 1 1 7 6 6 c c . . 
+. . . c c c c c c c c c c . . . 
+`,img`
+. . . . . c c c c c c c . . . . 
+. . . . c 6 7 7 7 7 7 6 c . . . 
+. . . c 7 c 6 6 6 6 c 7 6 c . . 
+. . c 6 7 6 f 6 6 f 6 7 7 c . . 
+. . c 7 7 7 7 7 7 7 7 7 7 c . . 
+. . f 7 8 1 f f 1 6 7 7 7 f . . 
+. . f 6 f 1 f f 1 f 7 7 7 f . . 
+. . . f f 2 2 2 2 f 7 7 6 f . . 
+. . c c f 2 2 2 2 7 7 6 f c . . 
+. c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. f 6 1 1 1 1 1 6 6 6 6 c . . . 
+. . f f c c c c c c c c . . . . 
+`,img`
+. . . . . . c c c c c c c . . . 
+. . . . . c f f 6 6 f f 7 c . . 
+. . . . c 7 6 6 6 6 6 6 7 6 c . 
+. . . c 7 7 7 7 7 7 7 7 7 7 c . 
+. . . c 7 8 1 f f 1 6 7 7 7 c . 
+. . . f 6 f 1 f f 1 f 7 7 7 f . 
+. . . f 6 f 2 2 2 2 f 7 7 7 f . 
+. . c c 6 f 2 2 2 2 f 7 7 6 f . 
+. c 7 7 7 7 2 2 2 2 7 7 f c . . 
+c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. f 6 1 1 1 1 6 6 6 6 6 c . . . 
+. . f f c c c c c c c c . . . . 
+`,img`
+. . . . . . c c c c c c c . . . 
+. . . . . c f f 6 6 f f 7 c . . 
+. . . . c 7 6 6 6 6 6 6 7 6 c . 
+. . . c 7 7 7 7 7 7 7 7 7 7 c . 
+. . . c 7 8 1 f f 1 6 7 7 7 c . 
+. . . f 6 f 1 f f 1 f 7 7 7 f . 
+. . . f 6 f 2 2 2 2 f 7 7 7 f . 
+. . c c 6 f 2 2 2 2 f 7 7 6 f . 
+. c 7 7 7 7 2 2 2 2 7 7 f c . . 
+c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. f 6 1 1 1 1 6 6 6 6 6 c . . . 
+. . f f c c c c c c c c . . . . 
+`,img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . c c c c c 
+. . . . . . . . . c c 7 7 7 6 c 
+. . . . . . . . c c 7 7 7 c c . 
+. . . . . . . . c 6 7 7 c . . . 
+. . . . . . . . c 6 6 6 c . . . 
+. . . c c c c c c 6 6 6 c c . . 
+. . c 6 7 7 7 7 6 c c 6 6 6 c . 
+. c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+. c 1 c f f 1 c 7 6 f 6 6 c c . 
+. c c c c c c c c c c c c . . . 
+`,img`
+. . . . . . . . . . . c c c c c 
+. . . . . . . . . c c 7 7 7 6 c 
+. . . . . . . . c c 7 7 7 c c . 
+. . . . . . . . c 6 7 7 c . . . 
+. . . . . . . . c 6 6 6 c . . . 
+. . . . . . . . c 6 6 6 c c . . 
+. . . c c c c c c c 6 6 6 c c . 
+. . c 6 7 7 7 7 6 c c 6 6 6 c . 
+. c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+. f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+. c 1 c f f 1 c 7 6 f 6 6 c c . 
+. c c c c c c c c c c c c . . . 
+`,img`
+. . . . . . . . . . . c c c c c 
+. . . . . . . . . c c 7 7 7 6 c 
+. . . . . . . . c c 7 7 7 c c . 
+. . . . . . . . c 6 7 7 c . . . 
+. . . . . . . . c 6 6 6 c . . . 
+. . . . . . . . c 6 6 6 c c . . 
+. . . c c c c c c c 6 6 6 c c . 
+. . c 6 7 7 7 7 6 c c 6 6 6 c . 
+. c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+. f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+. c 1 c f f 1 c 7 6 f 6 6 c c . 
+. c c c c c c c c c c c c . . . 
+`],
+        100,
+        true
+        )
+        bossDemon.follow(jesus.sprite)
+        bossMade = 1
+    }
+}
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.IdleLeft)
 })
 function setupGame () {
+    game.splash("Avoid demons and gather planets")
     hasDemon = 0
     firing = 0
     info.setScore(0)
     scene.setBackgroundColor(15)
     tiles.setTilemap(tiles.createTilemap(
-            hex`2000100001060606060606060606060606060606060606060b060606060606121313131302050505050505050505050505050505050505050b05050505050512130e0d1302050505050505050505050505050505050505050b050505050505121313131302050505050505050505050505050505050505050b05050505050512121010100b0b0b0b0b0b0b0b1005050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b0505050505050505050508020505050505100b0b0b0b0b0b0b0b0b050505051005050505050505050505080205050505050505050505050505100b050505050505050505050505050505080205050505050505050505050505050b050505050505050505050505050505080205050505050505050505050505050b050505050505050505050505050505080205050505050505050505050505050b05050505050505050505050505050508020505050505050505050505050505050505050505050505050505050505050802050505050505050505050505050505050505050505050505050505050505080310050505050505050505050505050505050505050505050505050505051009`,
+            hex`2000100001060606060606060606060606060606060606060b060606060606121313131302050505050505050505050505050505050505050b05050505050512130e0d1302050505050505050505050505050505050505050b050505050505121313131302050505050505050505050505050505050505050b05050505050512121010100b0b0b0b0b0b0b0b1005050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b050505050505050505050802050505050505050505050505050505050505050b0505050505050505050508020505050505100b0b0b0b0b0b0b0b0b050505051005050505050505050505080205050505050505050505050505100b050505050505050505050505050505080205050505050505050505050505050b050505050505050505050505050505080205050505050505050505050505050b050505050505050505050505050505080205050505050505050505050505050b05050505050505050505050505050508020505050505050505050505050505100505050505050505050505050505050802050505050505050505050505050505050505050505050505050505050505080310050505050505050505050505050505050505050505050505050505051009`,
             img`
 . . . . . . . . . . . . . . . . . . . . 2 . . . . . . 2 . . . . 
 . . . . . . . . . . . . . . . . . . . . 2 . . . . . . 2 . . . . 
@@ -203,8 +355,10 @@ function setupGame () {
     tiles.placeOnRandomTile(jesus.sprite, sprites.builtin.forestTiles10)
     controller.moveSprite(jesus.sprite)
     scene.cameraFollowSprite(jesus.sprite)
+    bossMade = 0
 }
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.setVelocity(0, 0)
     effects.playSmallHealingAnimation(sprite)
     pause(1000)
     sprite.destroy()
@@ -234,27 +388,24 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, f
     if (hasDemon == 0) {
         tiles.setTileAt(location, sprites.builtin.forestTiles10)
         info.changeScoreBy(1)
-    } else {
-    	
+    }
+    if (info.score() == 4) {
+        makeBossDemon()
     }
 })
 scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.hazardLava0, function (sprite, location) {
-    effects.playSmallHealingAnimation(sprite)
     sprite.destroy(effects.spray, 500)
     hasDemon = 0
     jesus.sprite.say("")
 })
 let hasDemon = 0
+let bossDemon: Sprite = null
 let firing = 0
 let jesus: Jesus = null
 let mySprite: Sprite = null
+let bossMade = 0
 setupGame()
 game.onUpdate(function () {
-    if (info.score() == 9) {
-        if (hasDemon == 0) {
-            game.over(true)
-        }
-    }
     if (hasDemon == 0) {
         jesus.sprite.say("")
     }
